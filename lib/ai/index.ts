2ae8005f0e6efe38283b0c1edddd0b3375451c44
liwenju <liwenjudetiankong@126.com>
2024-12-11 19:53:38 +0800

    完成openai base url设置，dev端口设置

--- lib/ai/index.ts
+++ lib/ai/index.ts
@@ -1,7 +1,11 @@
-import { openai } from '@ai-sdk/openai';
+import { createOpenAI } from '@ai-sdk/openai';
 import { experimental_wrapLanguageModel as wrapLanguageModel } from 'ai';
 
 import { customMiddleware } from './custom-middleware';
+
+const openai = createOpenAI({
+  baseURL: process.env.OPENAI_BASE_URL,
+});
 
 export const customModel = (apiIdentifier: string) => {
   return wrapLanguageModel({
